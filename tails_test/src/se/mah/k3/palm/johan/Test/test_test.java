e1de08e896871320774fa849f8d44bdb35bf43e3
tails <tails@10.1.1.29>
2011-04-01 10:25:46 +0200

    Signed-off-by: tails <tails@10.1.1.29>

---
+++ tails_test/src/se/mah/k3/palm/johan/Test/test_test.java
@@ -0,0 +1,13 @@
+package se.mah.k3.palm.johan.Test;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class test_test extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
