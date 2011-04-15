76536d7b8fa806e1f6f8a2454168de22e8285854
TailsQue <TailsQue@Worldender>
2011-04-15 15:18:34 +0200

    Signed-off-by: TailsQue <TailsQue@Worldender>

---
+++ whackamolle/src/se/mah/k3/palm/GameUpdateThread.java
@@ -0,0 +1,35 @@
+package se.mah.k3.palm;
+
+import android.os.Handler;
+import android.util.Log;
+
+public class GameUpdateThread extends Thread {
+	private Handler _ui;
+	private boolean _running = true;
+
+	public void stopCountDown() {
+		_running = false;
+	}
+
+	public GameUpdateThread(Handler ui) {
+		_ui = ui;
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		super.run();
+
+		while (_running) {
+			try {
+				Log.i("k3", "Thread Running");
+				_ui.sendEmptyMessage(NORM_PRIORITY);
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				Log.i("k3", e.getMessage());
+			}
+		}
+
+	}
+
+}
---
+++ whackamolle/src/se/mah/k3/palm/UpdateUI.java
@@ -0,0 +1,6 @@
+package se.mah.k3.palm;
+
+public interface UpdateUI {
+	void update(String message);
+
+}
