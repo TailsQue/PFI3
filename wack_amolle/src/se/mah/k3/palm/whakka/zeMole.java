ab973c201bde55ac78cd23eebdb33ffe229a997a
tails <tails@10.1.1.212>
2011-04-15 14:09:16 +0200

    Signed-off-by: tails <tails@10.1.1.212>
    Signed-off-by: TailsQue <TailsQue@Worldender>

---
+++ wack_amolle/src/se/mah/k3/palm/whakka/zeMole.java
@@ -0,0 +1,45 @@
+package se.mah.k3.palm.whakka;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.widget.Button;
+
+public class zeMole extends Button {
+	boolean alive = false;
+	List<Integer> myList;
+
+	public zeMole(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+		this.setText("");
+		myList = new ArrayList<Integer>();
+		myList.add(R.drawable.holewithoutmole);
+		myList.add(R.drawable.holewithmole);
+		setImage();
+	}
+
+	public void setImage() {
+		// TODO Auto-generated method stub
+		if (alive) {
+			alive = false;
+			this.setBackgroundResource(myList.get(0));
+			invalidate();
+		} else {
+			alive = true;
+			this.setBackgroundResource(myList.get(1));
+			invalidate();
+		}
+
+	}
+
+	protected void onDraw(Canvas canvas) {
+		// TODO Auto-generated method stub
+		super.onDraw(canvas);
+	}
+
+}
+
